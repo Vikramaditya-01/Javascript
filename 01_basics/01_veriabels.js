3151edcfea7efd99219edfc03bcff7d0d04f7841
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-23 21:25:58 +0000

    add detatype file

--- 01_basics/01_veriabels.js
+++ 01_basics/01_veriabels.js
@@ -1,1 +1,28 @@
 const accountId = 144553 ;
+let accountEmail = "abc@gmail.com";
+var accountPassword = "123qwerty";
+accountCity = "kolkata" ;
+
+
+// accountId = 2; // it's not allowed because it's constant
+accountEmail = "xyz@abc.com" ;
+accountPassword = "asdfgh"
+accountCity = "Gopiballavpur" 
+let accountState;
+
+
+console.log(accountId) ;
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+
+/*
+prefer not to use var 
+because of issue in block scope and functional scope
+*/
+
+console.table([accountId , accountEmail , accountPassword , accountCity ,accountState])   // print these same things in one line and print it's table 
+
+
+
+
