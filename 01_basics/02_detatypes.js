0598c752700e12f2f9d05c981345ef0b55236723
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-23 22:15:18 +0000

    Datatypes

---
+++ 01_basics/02_detatypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treate all js code as an newer version of javascript
+// alert(3+3); // we are using node js , not browser .
+
+
+let name = "vikramaditya";
+let age = 19
+let isLoggedIn = true 
+
+//number => 2 to the power 53
+// bigint
+//string => "" or ''
+// boolean => yes/no
+//null => standalone value
+// undefined 
+// Symbol => unique
+
+
+//object
+
+console.log(typeof undefined);
