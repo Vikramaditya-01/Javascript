f726bf7fba2ebe7a2117d94eecff85659c72746d
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-23 22:54:36 +0000

    Conversion of datatypes

---
+++ 01_basics/03_ConversionOperation.js
@@ -0,0 +1,45 @@
+let score = "vikramaditya"
+
+console.log(typeof(score));
+
+
+let valueInNumber = Number(score)  
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+// "vikramaditya" => NaN
+
+let isLoggedIn = 1 ;
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(typeof(booleanisLoggedIn));
+console.log(booleanisLoggedIn);
+
+
+//"" => false
+// null => false
+// undefined => false
+// 1 => true
+// 0 => false
+// "vikramaditya" => true
+
+
+let someNumber = "vikramaditya" ;
+
+let stringSomeNumber = String(someNumber);
+console.log(typeof(stringSomeNumber));
+console.log(stringSomeNumber);
+
+
+//"" => 
+// null => null
+// undefined => undefined
+// 1 => 1
+// 0 => 0
+// "vikramaditya" => vikramaditya
