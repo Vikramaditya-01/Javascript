22aab0efa6fa5f8d082cd63b69f2cf5ba45e26af
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-24 13:34:51 +0000

    ConversionOperation

--- 01_basics/03_ConversionOperation.js
+++ 01_basics/03_ConversionOperation.js
@@ -1,45 +1,69 @@
-let score = "vikramaditya"
+// let score = "vikramaditya"
 
-console.log(typeof(score));
-
-
-let valueInNumber = Number(score)  
-console.log(typeof(valueInNumber));
-console.log(valueInNumber);
-
-// "33" => 33
-// "33abc" => NaN
-// null => 0
-// undefined => NaN
-// true => 1
-// false => 0
-// "vikramaditya" => NaN
-
-let isLoggedIn = 1 ;
-
-let booleanisLoggedIn = Boolean(isLoggedIn);
-console.log(typeof(booleanisLoggedIn));
-console.log(booleanisLoggedIn);
+// console.log(typeof(score));
 
 
-//"" => false
-// null => false
-// undefined => false
-// 1 => true
-// 0 => false
-// "vikramaditya" => true
+// let valueInNumber = Number(score)  
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+// // "33" => 33
+// // "33abc" => NaN
+// // null => 0
+// // undefined => NaN
+// // true => 1
+// // false => 0
+// // "vikramaditya" => NaN
+
+// let isLoggedIn = 1 ;
+
+// let booleanisLoggedIn = Boolean(isLoggedIn);
+// console.log(typeof(booleanisLoggedIn));
+// console.log(booleanisLoggedIn);
 
 
-let someNumber = "vikramaditya" ;
+// //"" => false
+// // null => false
+// // undefined => false
+// // 1 => true
+// // 0 => false
+// // "vikramaditya" => true
 
-let stringSomeNumber = String(someNumber);
-console.log(typeof(stringSomeNumber));
-console.log(stringSomeNumber);
+
+// let someNumber = "vikramaditya" ;
+
+// let stringSomeNumber = String(someNumber);
+// console.log(typeof(stringSomeNumber));
+// console.log(stringSomeNumber);
 
 
-//"" => 
-// null => null
-// undefined => undefined
-// 1 => 1
-// 0 => 0
-// "vikramaditya" => vikramaditya
+// //"" => 
+// // null => null
+// // undefined => undefined
+// // 1 => 1
+// // 0 => 0
+// // "vikramaditya" => vikramaditya
+
+
+
+// //************************* Operations ***********************/
+
+
+// let value = 3
+// let negValue = -value ;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);  // ** for finding power here 2 to the power 2
+// console.log(2**3);   // here 2 to the power 3 
+
+// console.table([2+2 , 2-2 , 2*2 , 2**2 , 2**3])   /// printing the same value in table formate
+
+console.log(1 + "2");
+console.log("1" + 2);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(+true);
