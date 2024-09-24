848d675bd385d88968b81a3aab32db3669035faf
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-24 19:31:12 +0000

    string fist

---
+++ 01_basics/06_string.js
@@ -0,0 +1,18 @@
+const name = "Vikramaditya "   // decleare the string
+const repoCount = 10 
+console.log(`HEllo, My name is ${name} and My repo count is ${repoCount}`);
+
+
+const game = new String("Munmun");     // decleare the string .  (using object )
+
+console.log(game[0])   // printing the value of the perticular key of an object that we created
+// console.log(name + repoCount);
+
+console.log(game.__proto__)
+
+console.log(game.toUpperCase())
+console.log(game.charAt(2))
+console.log(game.indexOf('n'))
+
+
+
