2c0f959c3f63bfe6bf359429c9f1e3f52a7a06a8
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-25 01:40:45 +0000

    Number and Math

--- 01_basics/07_numbersAndMaths.js
+++ 01_basics/07_numbersAndMaths.js
@@ -1,14 +1,14 @@
-// const score = 400
-// console.log(score);
+const score = 400
+console.log(score);
 
-// const balance = new Number(100);
-// console.log(balance);
-// console.log(balance.toString());
-// let a = balance.toString()
-// console.log(typeof(a));
+const balance = new Number(100);
+console.log(balance);
+console.log(balance.toString());
+let a = balance.toString()
+console.log(typeof(a));
 
-// const otherNumber = 25.2354 
-// console.log(otherNumber.toPrecision(10));
+const otherNumber = 25.2354 
+console.log(otherNumber.toPrecision(10));
 
 
 // +++++++++++++++++++++++ => Maths <=++++++++++++++++++++
@@ -22,4 +22,4 @@ console.log(Math.random() * 10 + 1 );
 
 const min = 10
 const max = 20 
-console.log(Math.floor(Math.random() * (max - min +1)) +min )
+console.log(Math.floor(Math.random() * (max - min +1)) + min )
