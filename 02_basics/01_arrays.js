72c54870d2018f68fccf341bfa9141a16b7a633e
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-25 19:39:21 +0000

    array part-1

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -1,17 +1,41 @@
 // Array
 
-const myArray = [0 , 1 , 2 , 5 , 6 , 8 , 3 , 8 , 7 ]
+const myArray = [0 , 1 , 2 , 5 , 6 ]
 
 const myHeros = ["Iron Man" , "Thor" , "wanda"]
 
 const myArray2 = new Array(1 , 2 , 3 , 4);
 
 
-myArray.push(50)
+// myArray.push(50)
+// myArray.pop()
 
-console.log(myArray);
-console.log(myArray.length);
+// myArray.unshift(9)
+// myArray.shift()
+
+// console.log(myArray.includes(5))
+// console.log(myArray.indexOf(5))
+// console.log(myArray.lastIndexOf(8))
+// console.log(myArray.indexOf(8))
+
+// console.log(myArray);
+// console.log(myArray.length);
+
+// const newArray = myArray.join()
+// console.log(typeof newArray);
+// console.log( myArray);
 
 
+// sclice , slice
+ 
+console.log("A ", myArray);
 
+const mnya = myArray.slice(1,3)
+console.log(mnya);
 
+console.log("B ", myArray);
+
+const mnya2 = myArray.splice(1,3)
+console.log(mnya2);
+
+console.log("B ", myArray);
