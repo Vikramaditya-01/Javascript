57651bf6c8dbab98f240191b0f40aedc1b645bf1
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-25 15:04:18 +0000

    Array start

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,17 @@
+// Array
+
+const myArray = [0 , 1 , 2 , 5 , 6 , 8 , 3 , 8 , 7 ]
+
+const myHeros = ["Iron Man" , "Thor" , "wanda"]
+
+const myArray2 = new Array(1 , 2 , 3 , 4);
+
+
+myArray.push(50)
+
+console.log(myArray);
+console.log(myArray.length);
+
+
+
+
