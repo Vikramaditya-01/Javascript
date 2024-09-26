cbe3388b9012d73ad2217e962b431c27d9fa2895
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-26 11:38:51 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,16 @@
+function saymyname() {
+    console.log("V");
+    console.log("i");
+    console.log("k");
+    console.log("r");
+    console.log("a");
+    console.log("m");
+    console.log("a");
+    console.log("d");
+    console.log("i");
+    console.log("t");
+    console.log("y");
+    console.log("a");
+}
+
+saymyname()
