f28354c2a7ec7ab4dc42e0c3a9c41f3ca65b8443
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-28 19:43:57 +0000

    While and do-while Loop

---
+++ 05_itration/Do-WhileLoop.js
@@ -0,0 +1,24 @@
+// let Index = 0;
+// while (Index <= 10) {
+//     console.log(Index);
+//     Index++ ;
+// }
+
+// const myArray = ["Ironman" , "Thor" , "superman" , "Wonderwomen"]
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(myArray[arr]);
+// arr ++;
+// }
+
+
+
+////////////////// do-while loop ////////////////////////
+
+
+let score = 500
+
+do {
+    console.log(`The score is ${score}`);
+    score = score + 10;
+} while (score <= 100 );
