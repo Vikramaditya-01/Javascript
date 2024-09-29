bb3f07c1557be309916cea141997506b8201d642
Vikramaditya Gorai <146814652+Vikramaditya-01@users.noreply.github.com>
2024-09-29 15:01:57 +0000

    High Order Array Loop

--- 05_itration/high-order-ArrayLoop.js
+++ 05_itration/high-order-ArrayLoop.js
@@ -1,8 +1,39 @@
-// for of
+//for of
 
 const arr = [1 , 2 , 3 , 4 , 5 ]
 
 for (const num of arr) {
     console.log(num);
     
+}
+
+const greetings = "Hello World!"
+for(greet of greetings){
+    console.log(greet);
+}
+
+
+const map = new Map()
+
+map.set('IN' , "India")
+map.set('USA' , "United state of america")
+map.set('FR' , "France")
+map.set('RU' , "Russia")
+map.set('IN' , "India")
+
+// console.log(map);
+for (const [key , value] of map) {
+    console.log(key);
+    
+}
+
+const myObject = {
+    game1 : "GTA-V",
+    game2 : "Last 15 Days survive ",
+    game3 : "Free Fire ",
+    game4 : "Love is in small Things"
+}
+
+for (const [game] of myObject) {
+    console.log(`${game}`);
 }
---
+++ 05_itration/loop_in_object.js
@@ -0,0 +1,12 @@
+const myObject = {
+    game1 : "GTA-V",
+    game2 : "Last 15 Days survive ",
+    game3 : "Free Fire ",
+    game4 : "Love is in small Things"
+}
+
+for (const game in myObject) {
+       //console.log(myObject[game]);
+       console.log(`${game} is ${myObject[game]}`);
+       
+}
